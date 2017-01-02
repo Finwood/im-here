698537a2c25c2704282357e1ddcec589e2d83b85
Finwood <finwood@posteo.de>
2017-01-02 13:16:45 +0200

    added static routing rule

--- main.py
+++ main.py
@@ -2,11 +2,21 @@
 # coding: utf-8
 
 import os.path
+
 from eve import Eve
+from flask import send_from_directory
 
 SETTINGS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              'settings.py')
-app = Eve(__name__, settings=SETTINGS_PATH)
+app = Eve(__name__, settings=SETTINGS_PATH, static_url_path='')
+
+
+# the API is registered under the the URL prefix `/api`
+# let's use Eve as normal Flask application for serving statics
+@app.route('/static/<path:path>', methods=['GET'])
+def send_static(path):
+    return send_from_directory('static', path)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
