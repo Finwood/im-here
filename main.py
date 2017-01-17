599a9ad4898ca6c053cd01477cf24e1679abe3b3
Finwood <finwood@posteo.de>
2017-01-17 10:50:09 +0200

    get debug settins from env var

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.5
 # coding: utf-8
 
-import os.path
+import os
 
 from eve import Eve
 from flask import send_from_directory
@@ -9,6 +9,8 @@ from flask import send_from_directory
 SETTINGS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              'settings.py')
 app = Eve(__name__, settings=SETTINGS_PATH, static_url_path='')
+
+app.debug = 'DEBUG' in os.environ
 
 
 # the API is registered under the the URL prefix `/api`
