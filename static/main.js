46b9465e77313df0b1c559bfd19c29b9bba06277
Anticee <hulliton@gmail.com>
2017-01-19 21:30:38 +0200

    test

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 function updateLog() {
-    console.log("updating log...");
+    console.log("updating logee...");
 
     var newestEntry = $('.feed-container div.card:not(#card-template)').first().data('time');
     var data = newestEntry !== undefined ? {where: '{"_created": {"$gt": "' + newestEntry + '"}}'} : null;
@@ -28,15 +28,20 @@ function processRecord(rec) {
         $card.attr('id', rec._id);
         $card.find('p.ip').text(rec.user);
         $card.find('p.timestamp').text(rec._created);
-        $card.find('p.lat span').text(rec.lat);
-        $card.find('p.lon span').text(rec.lon);
+        $card.find('p.lat span').text(rec.lat.toFixed(2));
+        $card.find('p.lon span').text(rec.lon.toFixed(2));
         $card.data('time', rec._created);
 
-        $('.feed-container').prepend($card);
+        $('card:first-child').animate({
+            top: $card.height()
+        }, 1000, function () {
+            $('.feed-container').prepend($card);
+        });
     }
 }
 
 $(document).ready(function(){
     updateLog();
     var updateTimer = window.setInterval(updateLog, 2000);
+    console.log("test");
 });
